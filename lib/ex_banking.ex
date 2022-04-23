6e603d96d522c347f41c739c4cedb886c6b926d9
Kasun Edward <KasunE@omobio.net>
2022-04-23 23:53:26 +0530

    Start Account Process using Dynamic supervisor

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -3,6 +3,8 @@ defmodule ExBanking do
   def create_user(user) do
     case :ets.insert_new(:user_account, {user, %{}}) do
       true ->
+        start_spec = {ExBanking.Account, user}
+        DynamicSupervisor.start_child(ExBanking.AccountSup, start_spec)
         :ok
       _ ->
         :user_already_exists
