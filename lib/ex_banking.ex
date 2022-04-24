c8492ff56f69b19e4223ac73b39e846ad9f595a6
Kasun Edward <KasunE@omobio.net>
2022-04-24 14:27:12 +0530

    create_user/1 logic development

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -1,13 +1,25 @@
 defmodule ExBanking do
 
+  @spec create_user(user :: String.t) :: :ok | {:error, :wrong_arguments | :user_already_exists}
   def create_user(user) do
-    case :ets.insert_new(:user_account, {user, %{}}) do
-      true ->
-        start_spec = {ExBanking.Account, user}
-        DynamicSupervisor.start_child(ExBanking.AccountSup, start_spec)
-        :ok
-      _ ->
-        :user_already_exists
+    try do
+
+      if(!is_binary(user)) do
+        throw :wrong_arguments
+      end
+
+      case :ets.insert_new(:user_account, {user, %{"USD" =>0}}) do
+        true ->
+          start_spec = {ExBanking.Account, user}
+          DynamicSupervisor.start_child(ExBanking.AccountSup, start_spec)
+          :ok
+        _ ->
+          throw :user_already_exists
+      end
+    catch
+      error -> {:error, error}
+    end
+  end
     end
   end
 end
